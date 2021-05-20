386b9770cd49ba4f62265da6beb444b00d22909e
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2021-05-20 07:13:55 -0700

    Began new project
    This will focus on polymorphism. This is the beginning of review of how polymorphism works.

---
+++ CSIntermediatePolymorphism/CSIntermediatePolymorphism/Shape.cs
@@ -0,0 +1,26 @@
+﻿namespace CSIntermediatePolymorphism
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+
+    }
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
